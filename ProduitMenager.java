2309a6f076ea29dc0d892e377478b55c064aab85
paisible7 <22km127@esisalama.org>
2025-04-04 16:07:23 +0200

    COMMIT_04_AVRIL_2025 ==> implémentation de la sous classe produitMenager

--- ProduitMenager.java
+++ ProduitMenager.java
@@ -1,3 +1,33 @@
 public class ProduitMenager extends Produit {
+    private String typeProduit;
+    private String dateFabrication;
+
+    public ProduitMenager(int id, String nom, double prix, int quantite, String typeProduit, String dateFabrication) {
+        super(id, nom, prix, quantite);
+        this.typeProduit = typeProduit;
+        this.dateFabrication = dateFabrication;
+    }
+
+    public String getTypeProduit() {
+        return typeProduit;
+    }
+
+    public void setTypeProduit(String typeProduit) {
+        this.typeProduit = typeProduit;
+    }
+
+    public String getDateFabrication() {
+        return dateFabrication;
+    }
+
+    public void setDateFabrication(String dateFabrication) {
+        this.dateFabrication = dateFabrication;
+    }
+
+    @Override
+    public void afficherDetails() {
+        super.afficherDetails();
+        System.out.println("Type: " + typeProduit + ", Date de Fabrication: " + dateFabrication);
+    }
     
 }
